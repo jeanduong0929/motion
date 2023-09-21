e424219b5fd5a6afaa01bb0a3435c9c32cc566e4
bduong <bduong@accuregsoftware.com>
2023-09-21 13:25:48 -0400

    Moved todo dropdown to it's own component

---
+++ components/todo/todo-dropdown.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import Todo from "@/models/todo";
+import instance from "@/lib/axios-config";
+
+interface TodoDropdownProps {
+  todo: Todo;
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  setLoadingTodoId: React.Dispatch<React.SetStateAction<string>>;
+  setOpenDialog: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const TodoDropdown = ({
+  todo,
+  setTodos,
+  setLoadingTodoId,
+  setOpenDialog,
+}: TodoDropdownProps) => {
+  const handleDone = async (id: string, completed: boolean) => {
+    setLoadingTodoId(id);
+    try {
+      await instance.patch(`/todo/done`, {
+        id,
+        completed,
+      });
+
+      // Put line-through completed todos and move it to the bottom of the list
+      setTodos((prevTodos: Todo[]) => {
+        // Get the todo to be changed
+        const completedTodo = prevTodos.find((todo) => todo._id === id);
+
+        // Get the incompleted todos
+        const incompletedTodos = prevTodos.filter((todo) => todo._id !== id);
+
+        // Set the completed todo to the new completed value
+        completedTodo!.completed = completed;
+
+        // If the todo is completed
+        if (completed) {
+          // Moves the completed todo to the bottom of the list
+          return [...incompletedTodos, completedTodo!];
+        }
+        // Moves the undone todo to the top of the list
+        return [completedTodo!, ...incompletedTodos];
+      });
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      setLoadingTodoId("");
+    }
+  };
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="h-4 w-4 cursor-pointer"
+          >
+            <circle cx="12" cy="12" r="1"></circle>
+            <circle cx="12" cy="5" r="1"></circle>
+            <circle cx="12" cy="19" r="1"></circle>
+          </svg>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem
+            onClick={() => handleDone(todo._id, !todo.completed)}
+          >
+            {todo.completed ? "Undone" : "Done"}
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <Link href={`/dashboard/edit/${todo._id}`}>
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+          </Link>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className="text-red-500"
+            onClick={() => setOpenDialog(true)}
+          >
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export default TodoDropdown;
--- components/todo/todo-item.tsx
+++ components/todo/todo-item.tsx
@@ -1,17 +1,9 @@
 import Todo from "@/models/todo";
 import React from "react";
 import { useDrag, useDrop } from "react-dnd";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import Link from "next/link";
 import { Loader2 } from "lucide-react";
-import instance from "@/lib/axios-config";
 import TodoDeleteDialog from "./todo-delete-dialog";
+import TodoDropdown from "./todo-dropdown";
 
 interface TodoItemProps {
   todo: Todo;
@@ -44,40 +36,6 @@ const TodoItem = ({ todo, index, moveTodo, setTodos }: TodoItemProps) => {
     dropRef(node);
   };
 
-  const handleDone = async (id: string, completed: boolean) => {
-    setLoadingTodoId(id);
-    try {
-      await instance.patch(`/todo/done`, {
-        id,
-        completed,
-      });
-
-      // Put line-through completed todos and move it to the bottom of the list
-      setTodos((prevTodos: Todo[]) => {
-        // Get the todo to be changed
-        const completedTodo = prevTodos.find((todo) => todo._id === id);
-
-        // Get the incompleted todos
-        const incompletedTodos = prevTodos.filter((todo) => todo._id !== id);
-
-        // Set the completed todo to the new completed value
-        completedTodo!.completed = completed;
-
-        // If the todo is completed
-        if (completed) {
-          // Moves the completed todo to the bottom of the list
-          return [...incompletedTodos, completedTodo!];
-        }
-        // Moves the undone todo to the top of the list
-        return [completedTodo!, ...incompletedTodos];
-      });
-    } catch (error: any) {
-      console.log(error);
-    } finally {
-      setLoadingTodoId("");
-    }
-  };
-
   return (
     <>
       <div
@@ -106,44 +64,12 @@ const TodoItem = ({ todo, index, moveTodo, setTodos }: TodoItemProps) => {
             />
           </div>
         )}
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="h-4 w-4 cursor-pointer"
-            >
-              <circle cx="12" cy="12" r="1"></circle>
-              <circle cx="12" cy="5" r="1"></circle>
-              <circle cx="12" cy="19" r="1"></circle>
-            </svg>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem
-              onClick={() => handleDone(todo._id, !todo.completed)}
-            >
-              {todo.completed ? "Undone" : "Done"}
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <Link href={`/dashboard/edit/${todo._id}`}>
-              <DropdownMenuItem>Edit</DropdownMenuItem>
-            </Link>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              className="text-red-500"
-              onClick={() => setOpenDialog(true)}
-            >
-              Delete
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <TodoDropdown
+          todo={todo}
+          setTodos={setTodos}
+          setLoadingTodoId={setLoadingTodoId}
+          setOpenDialog={setOpenDialog}
+        />
       </div>
     </>
   );
