d20e7174ddb6b4e36949020fc046562fc67acafe
Jean Duong <jeanduong@Jeans-MBP.lan>
2023-09-26 12:06:39 -0400

    Organized the api/auth route.ts

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -4,40 +4,79 @@ import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const POST = async (req: NextRequest) => {
-  const { email, password } = await req.json();
+/**
+ * Handles POST requests for user authentication.
+ * @async
+ * @param {NextRequest} req - The Next.js request object.
+ * @returns {NextResponse} - A response object with the status of the operation and a JWT token if successful.
+ */
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
+  try {
+    // Extracting user credentials from the request body
+    const { email, password } = await req.json();
 
-  await connectDB();
+    // Connecting to the database
+    await connectDB();
 
-  const existingUser = await UserEntity.findOne({ email });
+    // Fetching the existing user from the database
+    const existingUser = await UserEntity.findOne({ email });
 
-  if (
-    !existingUser ||
-    !(
-      (await bcrypt.compare(password, existingUser.password)) ||
-      !password.trim()
-    )
-  ) {
-    return NextResponse.json({}, { status: 401 });
+    // Validating the provided credentials
+    if (!areValidCredentials(existingUser, password)) {
+      return NextResponse.json({}, { status: 401 });
+    }
+
+    // Generating a JWT token for the authenticated user
+    const token = generateToken(existingUser);
+
+    return NextResponse.json(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+        token,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    // Logging any unexpected errors
+    console.error(error);
+    return NextResponse.json({}, { status: 500 });
   }
+};
 
-  const token = jwt.sign(
-    {
-      id: existingUser.id,
-      email: existingUser.email,
-    },
-    process.env.JWT_SECRET as string,
-    {
-      expiresIn: "1h",
-    },
+/**
+ * Validates the provided user credentials.
+ * @async
+ * @param {any} existingUser - The existing user object from the database.
+ * @param {string} password - The password provided by the user.
+ * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the validity of the provided credentials.
+ */
+const areValidCredentials = async (
+  existingUser: any,
+  password: string,
+): Promise<boolean> => {
+  // Checking if the existing user is found and the provided password matches the stored password
+  return (
+    existingUser &&
+    password.trim() &&
+    (await bcrypt.compare(password, existingUser.password))
   );
+};
 
-  return NextResponse.json(
-    {
-      id: existingUser.id,
-      email: existingUser.email,
-      token,
-    },
-    { status: 200 },
-  );
+/**
+ * Generates a JWT token for the authenticated user.
+ * @param {any} user - The authenticated user object.
+ * @returns {string} - A JWT token.
+ */
+const generateToken = (user: any): string => {
+  // Defining the payload for the JWT token
+  const payload = {
+    id: user.id,
+    email: user.email,
+  };
+
+  // Signing and returning the JWT token
+  return jwt.sign(payload, process.env.JWT_SECRET as string, {
+    expiresIn: "1h",
+  });
 };
--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,38 +1,84 @@
 import UserEntity from "@/entities/user-entity";
+import connectDB from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt";
-import connectDB from "@/lib/db";
 
-export const POST = async (req: NextRequest) => {
-  const { email, password } = await req.json();
+/**
+ * Handles POST requests for user registration.
+ * @async
+ * @param {NextRequest} req - The Next.js request object.
+ * @returns {NextResponse} - A response object with the status of the operation.
+ */
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
+  try {
+    // Extracting user details from the request body
+    const { email, password } = await req.json();
 
-  await connectDB();
+    // Connecting to the database
+    await connectDB();
 
-  if (!isValidEmail(email) || !isValidPassword(password)) {
-    return NextResponse.json({}, { status: 400 });
+    // Validating the provided email and password
+    if (!isValidEmail(email) || !isValidPassword(password)) {
+      return NextResponse.json({}, { status: 400 });
+    }
+
+    // Checking if a user with the provided email already exists
+    if (await UserEntity.findOne({ email })) {
+      return NextResponse.json({}, { status: 409 });
+    }
+
+    // Creating a new user with the provided details
+    await createUser(email, password);
+
+    return NextResponse.json({}, { status: 201 });
+  } catch (error) {
+    // Logging any unexpected errors
+    console.error(error);
+    return NextResponse.json({}, { status: 500 });
   }
+};
 
-  if (await UserEntity.findOne({ email })) {
-    return NextResponse.json({}, { status: 409 });
-  }
+/**
+ * Validates the structure of the provided email.
+ * @param {string} email - The email to validate.
+ * @returns {boolean} - A boolean indicating whether the email is valid.
+ */
+const isValidEmail = (email: string): boolean => {
+  // Defining the regex pattern for a valid email
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+  // Testing the provided email against the pattern
+  return emailRegex.test(email);
+};
+
+/**
+ * Validates the structure of the provided password.
+ * @param {string} password - The password to validate.
+ * @returns {boolean} - A boolean indicating whether the password is valid.
+ */
+const isValidPassword = (password: string): boolean => {
+  // Defining the regex pattern for a valid password
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  // Testing the provided password against the pattern
+  return passwordRegex.test(password);
+};
+
+/**
+ * Creates a new user with the provided email and password.
+ * @async
+ * @param {string} email - The email of the new user.
+ * @param {string} password - The password of the new user.
+ * @returns {Promise<void>} - A promise that resolves when the user has been created.
+ */
+const createUser = async (email: string, password: string): Promise<void> => {
+  // Generating a new salt for password hashing
   const salt = await bcrypt.genSalt(10);
+
+  // Hashing the provided password with the generated salt
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  await UserEntity.create({
-    email,
-    password: hashedPassword,
-  });
-
-  return NextResponse.json({}, { status: 201 });
-};
-
-const isValidEmail = (email: string) => {
-  return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
-};
-
-const isValidPassword = (password: string) => {
-  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
-    password,
-  );
+  // Creating a new user entity with the hashed password
+  await UserEntity.create({ email, password: hashedPassword });
 };
