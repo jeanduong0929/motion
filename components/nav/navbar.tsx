2a1d1f1bfab4678bb96012074b3d03fcdbecfb5e
Jean Duong <jeanduong@Jeans-MBP.lan>
2023-09-26 14:47:31 -0400

    Revert the navbar change

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
+import Loading from "@/components/loading";
 
 // Components
 import UserDropdown from "./userdropdown";
@@ -12,17 +13,15 @@ import { Button } from "../ui/button";
 // Icons
 import { CommandIcon } from "lucide-react";
 
-/* ##############################NavbarContainer ############################## */
-
 /**
- * Container component for the navbar.
- *
- * @param {React.FC<NavbarContainerProps>} props - Props for the NavbarContainer component.
+ * Component for rendering the navbar container.
+ * @returns {JSX.Element} The navbar container component.
  */
-interface NavbarContainerProps {
-  children: React.ReactNode;
-}
-const NavbarContainer = ({ children }: NavbarContainerProps) => {
+const Navbar = (): JSX.Element => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") return <Loading />;
+
   return (
     <div className="w-full border-b container-fade-in">
       <nav className="flex items-center justify-between py-5 mx-auto w-11/12 max-w-screen-xl">
@@ -33,47 +32,17 @@ const NavbarContainer = ({ children }: NavbarContainerProps) => {
 
         <div className="flex items-center gap-10">
           <ModeToggle />
-          {children}
+          {session ? (
+            <UserDropdown />
+          ) : (
+            <Link href={"/login"}>
+              <Button>Login</Button>
+            </Link>
+          )}
         </div>
       </nav>
     </div>
   );
-};
-
-/* ##############################Navbar ############################## */
-
-/**
- * The main Navbar component.
- */
-const Navbar = () => {
-  const { data: session, status } = useSession();
-
-  // If the session status is still loading, return null to avoid rendering the navbar prematurely.
-  if (status === "loading") return null;
-
-  // If there's no session, render a login button.
-  if (!session) {
-    return (
-      <>
-        <NavbarContainer>
-          <Link href={"/login"}>
-            <Button>Login</Button>
-          </Link>
-        </NavbarContainer>
-      </>
-    );
-  }
-
-  // If a session exists, render the UserDropdown component.
-  if (session) {
-    return (
-      <>
-        <NavbarContainer>
-          <UserDropdown />
-        </NavbarContainer>
-      </>
-    );
-  }
 };
 
 export default Navbar;
